67f4ff806571419a0a98a8d4fe7fb3df5bbfd9f6
saraperera2021 <saraperera2021@gmail.com>
2024-10-31 11:46:15 +0530

    Defined endpoints for add, delete and update books

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -32,6 +32,21 @@ object Main extends IOApp {
     .in("list" / "all")
     .out(jsonBody[Vector[Book]])
 
+  val addBookEndpoint: PublicEndpoint[Book, Unit, String, Any] = endpoint.post
+    .in("books" / "add")
+    .in(jsonBody[Book])
+    .out(stringBody)
+
+  val deleteBookEndpoint: PublicEndpoint[String, String, String, Any] = endpoint.delete
+    .in("books" / "delete" / path[String]("title"))
+    .errorOut(stringBody)
+    .out(stringBody)
+
+  val updateBookEndpoint: PublicEndpoint[(String, Book), Unit, String, Any] = endpoint.put
+    .in("books" / "update" / path[String]("title")) // Use path parameter for the book title
+    .in(jsonBody[Book])
+    .out(stringBody)
+
   val books = new AtomicReference(
     Vector(
       Book("The Sorrows of Young Werther", 1774, Author("Johann Wolfgang von Goethe")),
@@ -54,12 +69,48 @@ object Main extends IOApp {
     booksListingEndpoint.serverLogicSuccess(_ => IO(books.get()))
   )
 
+  val addBookRoutes: HttpRoutes[IO] =
+    Http4sServerInterpreter[IO]().toRoutes(
+      addBookEndpoint.serverLogicSuccess { newBook =>
+        IO {
+          books.updateAndGet(_ :+ newBook)
+          s"Book '${newBook.title}' by ${newBook.author.name} added successfully!"
+        }
+      })
+
+  val deleteBookRoutes: HttpRoutes[IO] =
+    Http4sServerInterpreter[IO]().toRoutes(
+      deleteBookEndpoint.serverLogic { title =>
+        IO {
+          val updatedBooks = books.get().filterNot(_.title.equalsIgnoreCase(title))
+          if (updatedBooks.size == books.get().size) {
+            Left(s"Book with title '$title' not found")
+          } else {
+            books.set(updatedBooks)
+            Right(s"Book with title '$title' deleted successfully!")
+          }
+        }
+      })
+
+  val updateBookRoutes: HttpRoutes[IO] =
+    Http4sServerInterpreter[IO]().toRoutes(
+      updateBookEndpoint.serverLogicSuccess { case (title, updatedBook) =>
+        IO {
+          val updatedBooks = books.get().map {
+            case book if book.title == title => updatedBook
+            case book => book
+          }
+          books.set(updatedBooks)
+          s"Book '${updatedBook.title}' updated successfully!"
+        }
+      })
+
   // generating and exposing the documentation in yml
   val swaggerUIRoutes: HttpRoutes[IO] =
     Http4sServerInterpreter[IO]().toRoutes(
-      SwaggerInterpreter().fromEndpoints[IO](List(helloEndpoint, goodbyeEndpoint, booksListingEndpoint), "My API", "1.0.0")
+      SwaggerInterpreter().fromEndpoints[IO](List(helloEndpoint, goodbyeEndpoint, booksListingEndpoint, addBookEndpoint, deleteBookEndpoint,updateBookEndpoint), "My API", "1.0.0")
     )
-  val routes: HttpRoutes[IO] = helloRoutes <+> goodbyeRoutes <+> booksListingRoutes <+> swaggerUIRoutes
+  val routes: HttpRoutes[IO] = helloRoutes <+> goodbyeRoutes <+> booksListingRoutes <+> addBookRoutes <+> deleteBookRoutes <+> updateBookRoutes <+>  swaggerUIRoutes
 
   override def run(args: List[String]): IO[ExitCode] = {
     EmberServerBuilder
